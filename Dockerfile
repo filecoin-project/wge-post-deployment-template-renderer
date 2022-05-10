92df1521941491a21ef02a871abc9cc20def1b3c
Steven Fraser <steve.fraser@weave.works>
2022-05-10 07:59:20 -0400

    adding finished dockerfile and command

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,12 @@
+FROM dannyben/bashly
+WORKDIR /home/
+ADD . /home/
+RUN bashly generate
+
 FROM hairyhenderson/gomplate:v3.10.0-alpine
+WORKDIR /root/
+RUN apk update && apk upgrade && apk add git
+RUN apk update && apk upgrade && apk add curl
+RUN apk update && apk upgrade && apk add bash
+COPY --from=0 /home/cli ./
+ENTRYPOINT ["./cli"]  
---
+++ src/template_command.sh
@@ -0,0 +1,32 @@
+echo "# this file is located in 'src/template_command.sh'"
+echo "# code for 'cli template' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
+set -a
+
+git clone -b ${GITOPS_REPO_BRANCH} https://github.com/${GITOPS_REPO_OWNER}/${GITOPS_REPO_NAME} /tmp/${GITOPS_REPO_NAME}
+cd /tmp/${GITOPS_REPO_NAME}
+if [ ! -f "${TEMPLATE_PATH}" ]; then
+    echo "Template does not exist"
+    rm -rf /tmp/${GITOPS_REPO_NAME}
+    exit 1
+fi
+
+if [[ ! -z "${ENV_HELPER_SCRIPT}" ]]; then
+    curl ${ENV_HELPER_SCRIPT} --output /tmp/env_helper.sh
+    chmod +x /tmp/env_helper.sh
+    sh /tmp/env_helper.sh
+fi
+
+if [[ ! -z "${ENV_CONFIG_FILE_PATH}" ]]; then
+    source $ENV_CONFIG_FILE_PATH
+fi
+
+gomplate --file ${TEMPLATE_PATH} --out ${TEMPLATE_PATH}.tmp
+mv ${TEMPLATE_PATH}.tmp ${TEMPLATE_PATH}
+
+git add ${TEMPLATE_PATH}
+git commit -m "Updating variables"
+git push
+
+rm -rf /tmp/${GITOPS_REPO_NAME}
